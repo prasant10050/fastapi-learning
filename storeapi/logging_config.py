1f4c5b949474a0a645bd25c456807acf7f86572a
Prasant <prasant10050@gmail.com>
2024-03-24 16:58:29 +0530

    WIP

--- storeapi/logging_config.py
+++ storeapi/logging_config.py
@@ -13,19 +13,33 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levename)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024,  # 1MB
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": "INFO"},
+                "uvicorn": {"handlers": ["default","rotating_file"], "level": "INFO"},
                 "storeapi": {
-                    "handlers": ["default"],
+                    "handlers": ["default","rotating_file"]],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
                 },
--- storeapi/main.py
+++ storeapi/main.py
@@ -1,7 +1,8 @@
 import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 
 from storeapi.database import database
 from storeapi.logging_config import configure_logging
@@ -13,15 +14,18 @@ logger = logging.getLogger(__name__)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     configure_logging()
-    logger.info("Database conneting...")
     await database.connect()
-    logger.info("Database connected.")
     yield
     await database.disconnect()
-    logger.info("Database disconnected.")
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(post_router)
 # app.include_router(post_router,prefix="/posts")
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handle_loggng(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    return await http_exception_handler(request, exc)
